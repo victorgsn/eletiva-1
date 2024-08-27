30186229ecc4083976e792b4a6be43e050f39f74
victorgsn <149541202+victorgsn@users.noreply.github.com>
2024-08-27 20:29:16 -0300

    laço de repetição

---
+++ Aula2/exemplo2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desvios Condicionais</title>
+</head>
+<body>
+    <form action="exemplo2_resposta.php" method="POST">
+    <input type="number" name="valor">
+    <button type="submit">ok</button> 
+
+    </form>
+</body>
+</html>
